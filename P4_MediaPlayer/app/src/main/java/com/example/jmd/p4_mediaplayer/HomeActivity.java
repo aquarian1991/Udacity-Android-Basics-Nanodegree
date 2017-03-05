d23d23d51236d500d5f233e8a58d64f17185fd20
aquarian1991 <aquarian1991@gmail.com>
2017-03-05 19:11:34 +0530

    Adding 4th project of the course. App modeling the structure of a media player.

---
+++ P4_MediaPlayer/app/src/main/java/com/example/jmd/p4_mediaplayer/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.jmd.p4_mediaplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+/**
+ * Created by JMD on 04/03/17.
+ */
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Home");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
